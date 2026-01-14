5d0027a591ad83de17f3617efbb6c39fc175e02a
mshayankhan-11 <mshayankhan339@gmail.com>
2026-01-14 17:40:50 +0500

    benefits changes

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,14 +1,15 @@
 // app/components/WhyChooseUs.tsx
 
 import InventoryPart from "@/components/InventoryPart";
-import WhyChooseUs from "@/components/WhyChooseUs";
+// import WhyChooseUs from "@/components/Benefits";
 import Image from "next/image";
+import Benefits from "@/components/Benefits";
 
 export default function Shop() {
   const data = Array.from({ length: 10 }, (_, i) => i + 1);
   return (
-    <div className="flex flex-col">
-      <WhyChooseUs />
+    <div className="flex flex-col bg-white">
+      <Benefits />
       <div className="flex flex-wrap gap-5 items-center justify-items-start">
         {data.map((item) => (
           <div key={item}>
