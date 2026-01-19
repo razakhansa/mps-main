129741bb06bebcf8e62950b2a8153aceec2e86fc
Hamayun-001 <usmankhan7586@gmail.com>
2026-01-19 15:31:38 +0500

    Add Navbar component to RootLayout and create LoginPage component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,4 @@
 import DiscountBanner from "@/components/DiscountBanner";
-"use client";
-
-import Faq from "@/components/FAQ";
 import LoginForm from "@/components/LoginForm";
 import CatalogBar from "@/components/CatalogBar";
 import Faq from "@/components/FAQ";
@@ -12,10 +9,8 @@ export default function Home() {
     <div className="flex min-h-screen items-center justify-center  font-sans">
       <main className="flex min-h-screen w-full  flex-col items-center justify-between bg-white  sm:items-start">
         <CatalogBar />
-        <Navbar />
         <Faq />
-        <DiscountBanner/>
-        <LoginForm/>
+        <DiscountBanner />
       </main>
     </div>
   );
