efc26a0a4e317931b0a1e71bcdac5f75b22edc6b
razakhansa <mrazakhan8921@gmail.com>
2026-01-22 03:38:12 +0500

    Non Catalog Item form done

---
+++ app/cart/page.tsx
@@ -0,0 +1,12 @@
+import NonCatalogItemForm from "@/components/NonCatalogItemForm";
+import React from "react";
+
+function page() {
+  return (
+    <div className="w-full p-8 my-8">
+      <NonCatalogItemForm />
+    </div>
+  );
+}
+
+export default page;
